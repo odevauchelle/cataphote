0717801ee4ea07ed197f37188c35cd80e3289821
Olivier Devauchelle <olivier@leechong.ipgp.fr>
2022-11-07 16:09:28 +0100

    Float computation without sympy

--- cataphote/cataphote.py
+++ cataphote/cataphote.py
@@ -19,9 +19,56 @@
 
 import numpy as np
 from matplotlib.pyplot import gca
-from sympy import Ray, Segment, intersection
+# import sympy as sp
 
 # from shapely.geometry import LineString
+
+class Segment :
+
+    def __init__( self, p1, p2) :
+        self.p1 = np.array( p1 )
+        self.p2 = np.array( p2 )
+
+    def contains( self, point, tol = 1e-14 ) :
+
+        return abs( np.linalg.norm( self.p1 - self.p2 ) - np.linalg.norm( self.p1 - point ) - np.linalg.norm( point - self.p2 ) ) < tol
+
+
+class Ray :
+
+    def __init__( self, p1, p2 = None, angle = None ) :
+
+        self.p1 = np.array( p1 )
+
+        if p2 is None :
+            self.p2 = self.p1 + np.array( [np.cos(angle), np.sin(angle)] )
+        else :
+            self.p2 = np.array( p2 )
+
+
+    def get_angle( self ) :
+        return np.angle( np.dot( self.p2 - self.p1, np.array( [ 1, 1j  ] ) ) )
+
+
+    def intersection( self, segment ) :
+
+        M = np.array( [ self.p2 - self.p1, segment.p1 - segment.p2 ] ).T
+        B = np.array( [ segment.p1 - self.p1 ] ).T
+        #
+        try :
+            c, _ = np.linalg.solve( M, B )
+
+            x = c*( self.p2 - self.p1 ) + self.p1
+
+            if c >= 0 and segment.contains( x ) :
+                return x
+
+            else :
+                return None
+
+        except :
+            return None
+
 
 class cataphote :
 
@@ -52,7 +99,9 @@ class cataphote :
         for i, segment in enumerate( self.segments ) :
             try :
                 if not segment.contains( ray.p1 ) :
-                    intersections[i] = intersection( ray, segment )[0]
+                    intersection_point = ray.intersection( segment )
+                    if not intersection_point is None :
+                        intersections[i] = intersection_point
             except :
                 pass
 
@@ -67,7 +116,7 @@ class cataphote :
         if len(intersections) > 0 :
 
             for i, point in intersections.items() :
-                distances += [ point.distance( ray.p1 ) ]
+                distances += [ np.linalg.norm( point - ray.p1 ) ]
                 segment_indices += [i]
 
             segment_index = segment_indices[ np.argmin( distances ) ]
@@ -87,9 +136,9 @@ class cataphote :
 
         traj = []
 
-        while len(traj) <= max_iterations :
+        while True :
 
-            traj += [ ray.p1 ]
+            traj += [ list( ray.p1 ) ]
 
             new_ray = self.get_reflection( ray )
 
@@ -97,6 +146,10 @@ class cataphote :
                 break
             else :
                 ray = new_ray
+
+            if len(traj) > max_iterations :
+                print('Too many iterations, len(traj)>', max_iterations)
+                break
 
         return traj, ray
 
@@ -110,21 +163,35 @@ if __name__ == '__main__' :
 
     from pylab import *
 
+    ax = gca()
+
+    # s = Segment( [0,1], [1,.9] )
+    # r = Ray( [.5,1], [.01,.01] )
+    #
+    # i = r.intersection(s)
+    #
+    # for stuff in (s,r) :
+    #     ax.plot( *array( [ stuff.p1, stuff.p2 ] ).T, label = type( stuff ) )
+    #
+    # try :
+    #     ax.plot( *i, 'o' )
+    # except :
+    #     print(i)
+
+
     c = cataphote( [ (-.5,0), (-.9,-.7), (0,-.4), (1.2,-.8), (.5,0), (-.5,0) ] )
 
 
-    ax = gca()
     c.plot_reflector( ax = ax, color = 'k', lw = 2 )
 
     for angle in -0.2*array( [1]  )*np.pi/2 :
 
-        x = np.array([0,0])
+        x = np.array( [ 0, 0 ] )
 
         ray = Ray( x, angle = angle )
 
         traj, ray = c.get_trajectory( ray, max_iterations = 150 )
 
-        # plot( *np.array( traj + [ ray.p2 ] ).T , label = str(len(traj)))
         plot( *np.array( traj ).T, label = str( len(traj) ), alpha = 1, lw = .3 )
 
     ax.axis('equal')
@@ -133,6 +200,6 @@ if __name__ == '__main__' :
     ax.set_xticks([])
     ax.set_yticks([])
 
-    savefig('billiard.svg', bbox_inches = 'tight')
+    # savefig('billiard.svg', bbox_inches = 'tight')
 
     show()
